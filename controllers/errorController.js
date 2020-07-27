02e3a1a8384ff51d60fa0885eb5a96e55d73a7a5
Siya Sharma <siyaduttsharma@gmail.com>
2020-07-27 12:40:54 +0530

    Added changes for deployment

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -9,7 +9,6 @@ const handleCastErrorDB = err => {
 //Handling duplicate keys error
 const handleDuplicateFieldsDB = err => {
   const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
-  console.log(value);
 
   const message = `Duplicate field value: ${value}. Please use another value!`;
   return new AppError(message, 400);
